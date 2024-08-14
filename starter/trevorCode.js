a6bfe4d2496d7a164923fecfb569665f9ffd95be
Trevor Waters <twater623@west-mec.org>
2024-08-14 14:26:16 -0700

    added trevcode

---
+++ starter/trevorCode.js
@@ -0,0 +1,52 @@
+const prompt = require("prompt-sync")();
+// require gravityFactors from "./gravityFactors.js"
+const gravityFactors = require("./gravityFactors.js");
+// create a function called calculateWeightOnPlanets
+
+// pass an argument of earthWeight
+function calculateWeight(earthWeight) {
+  // inside the function initialize an empty object
+  const planetWeights = {};
+  // the object is called planetWeights
+  // iterate over gravityFactors
+  for (let planet in gravityFactors) {
+    // assign a key, value pair to planetWeights for each iteration
+    planetWeights[planet] = parseFloat(
+      (earthWeight * gravityFactors[planet]).toFixed(2)
+    );
+
+    // planetWeights[planet] = Math.round((earthWeight * gravityFactors[planet]) * 100) / 100;
+    // the value of each key should be earthWeight times the value of the current
+    // iteration of gravityFactors
+    // Make sure the values are floats to two decimals
+  }
+  for (let planet in planetWeights) {
+    console.log(
+      "Your weight on",
+      `${planet}`,
+      "is",
+      `${planetWeights[planet]}`,
+      "kg"
+    );
+  }
+  // return the planetWeights object from the function
+  //   console.log(planetWeights);
+}
+function getUserWeight() {
+  console.log("Enter your weight in kg");
+  const userWeight = prompt(">");
+  console.log("Your weight is:", userWeight);
+  // create a method to take the user weight
+  // and log the weight on other planets
+  console.log("Your weight on other planets is:");
+  calculateWeight(userWeight);
+}
+global.getUserWeight = getUserWeight;
+// console.log("Your weight on other planets is:");
+// console.log(calculateWeight(100));
+// console log the planetWeights assume the function
+// is passed in an earthWeight of 100(kg)
+
+// make a comment at the bottom of the script
+// telling a user how to run the script from node
+// run in the terminal `node calculateWeight.js`
