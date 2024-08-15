99e57939c1b3ef684222831f2657535cdcb4d429
Trevor Waters <twater623@west-mec.org>
2024-08-15 13:52:05 -0700

    2pm

--- starter/startRepl.js
+++ starter/startRepl.js
@@ -7,37 +7,38 @@
 // Command to run this script with nodemon
 // `nodemon --exec "node" startRepl.js`
 
-const repl = require('repl');
-const fs = require('fs');
-const path = require('path');
+const repl = require("repl");
+const fs = require("fs");
+const path = require("path");
+// const prompt = require("node-sync");
 
-const scripts = ['./index.js'];
+const scripts = ["./index.js", "./finalCode.js"];
 
 const loadFunctions = () => {
-    scripts.forEach(script => {
-        try {
-            delete require.cache[require.resolve(script)];
-            require(script);
-            console.log(`${script} loaded successfully`);
-        } catch (error) {
-            console.error(`Error loading ${script}:`, error);
-        }
-    });
-}
+  scripts.forEach((script) => {
+    try {
+      delete require.cache[require.resolve(script)];
+      require(script);
+      console.log(`${script} loaded successfully`);
+    } catch (error) {
+      console.error(`Error loading ${script}:`, error);
+    }
+  });
+};
 
 loadFunctions();
 
-const replServer = repl.start({ prompt: '> ' });
+const replServer = repl.start({ prompt: "> " });
 
-scripts.forEach(script => {
-    fs.watchFile(path.resolve(__dirname, script), (curr, prev) => {
-        console.log(`${script} changed, reloading...`);
-        try {
-            delete require.cache[require.resolve(script)];
-            require(script);
-            console.log(`${script} reloaded successfully`);
-        } catch (error) {
-            console.error(`Error reloading ${script}:`, error);
-        }
-    });
+scripts.forEach((script) => {
+  fs.watchFile(path.resolve(__dirname, script), (curr, prev) => {
+    console.log(`${script} changed, reloading...`);
+    try {
+      delete require.cache[require.resolve(script)];
+      require(script);
+      console.log(`${script} reloaded successfully`);
+    } catch (error) {
+      console.error(`Error reloading ${script}:`, error);
+    }
+  });
 });
