390e129473db5fe9a23e8b00c63ca77929fe39de
Trevor Waters <twater623@west-mec.org>
2024-08-15 14:27:41 -0700

    finish

--- starter/finalCode.js
+++ starter/finalCode.js
@@ -1,14 +1,21 @@
-const prompt = require("prompt-sync");
+const prompt = require("prompt-sync")();
 
 const gravityFactors = require("./utils/earthGravityFactors.js");
+const alienFactors = require("./utils/alienGravityFactors.js");
 
-function showUserFactors(type, value) {
+function showUserFactors(system, type, value) {
   let results = {};
+  let alienResults = {};
 
   let measurement;
 
   for (let planet in gravityFactors) {
     results[planet] = parseFloat((gravityFactors[planet] * value).toFixed(2));
+  }
+  for (let planet in alienFactors) {
+    alienResults[planet] = parseFloat(
+      (alienFactors[planet] * value).toFixed(2)
+    );
   }
   switch (type) {
     case "jump":
@@ -29,5 +36,17 @@ function showUserFactors(type, value) {
     );
   }
 }
+
+function getUserInput() {
+  console.log("Which solar system do you want? 1 or 2?");
+  let userPlanet = prompt(">>");
+  console.log("Enter Type");
+  let userType = prompt(">> ");
+  console.log("Enter Value");
+  let userValue = prompt(">> ");
+
+  showUserFactors(userPlanet, userType, userValue);
+}
+global.getUserInput = getUserInput;
 
 global.showUserFactors = showUserFactors;
