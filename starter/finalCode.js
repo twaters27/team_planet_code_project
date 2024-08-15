c28e1c6b7fe997ea4c5d82ed26d953b7b29c2cc1
Trevor Waters <twater623@west-mec.org>
2024-08-15 13:09:21 -0700

    function

---
+++ starter/finalCode.js
@@ -0,0 +1,18 @@
+const prompt = require("node-sync")();
+
+const gravityFactors = require("./utils/earthGravityFactors.js");
+
+function showUserFactors(type, value) {
+  let results = {};
+
+  let measurement;
+
+  for (let planet in gravityFactors) {
+    results[planet] = parseFloat((gravityFactors[planet] * value).toFixed(2));
+  }
+  switch (type) {
+    case "jump":
+
+    case "weight":
+  }
+}
